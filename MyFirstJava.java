81a3d711f723418f93f515788534fbff73b336c3
Ilham Khalis Putra Agatha <42014071+IlhamKhalis@users.noreply.github.com>
2021-10-14 23:42:51 +0700

    Add files via upload

---
+++ MyFirstJava.java
@@ -0,0 +1,24 @@
+public class MyFirstJava{
+    public static void main(String[] args){
+        // System.out.print("Hello World");
+        // System.out.print("Saya suka pemrograman");
+
+        String a = "---------------------------------------------";
+        String b = "Biodata Mahasiswa Jurusan Teknologi Informasi";
+        String c = "Nama    : Ilham Khalis Putra Agatha";
+        String d = "NIM     : ";
+        int e    = 2141720230;
+        String f = "Alamat  : Malang";
+        String g = "No HP   : 081217768251";
+        String h = "Hobi    : Bermain";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(d+e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
